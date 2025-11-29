c5a6442b929b92e585165b3c29759aed1fb78065
Sandesh Sonel <sonelsandesh@gmail.com>
2025-11-29 20:06:15 +0530

    fix: bug report button added on auth page

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,13 +1,21 @@
 import { type QueryClient } from '@tanstack/react-query'
-import { createRootRouteWithContext, Outlet } from '@tanstack/react-router'
+import {
+  createRootRouteWithContext,
+  Outlet,
+  useLocation,
+} from '@tanstack/react-router'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
 import { Toaster } from '@/components/ui/sonner'
+import BugReport from '@/components/bug-report'
 import { NavigationProgress } from '@/components/navigation-progress'
 import { GeneralError } from '@/features/errors/general-error'
 import { NotFoundError } from '@/features/errors/not-found-error'
 
+const bugReportPath = ['/sign-in', '/sign-up']
+
 function RootComponent() {
+  const { pathname } = useLocation()
   return (
     <div className='relative'>
       <NavigationProgress />
@@ -18,6 +26,12 @@ function RootComponent() {
           <ReactQueryDevtools buttonPosition='bottom-left' />
           <TanStackRouterDevtools position='bottom-right' />
         </>
+      )}
+
+      {bugReportPath.includes(pathname) && (
+        <div className='absolute right-6 bottom-6'>
+          <BugReport />
+        </div>
       )}
     </div>
   )
